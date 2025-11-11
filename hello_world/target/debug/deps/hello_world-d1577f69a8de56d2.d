3c5cd31c665b9274f3731dfc09ce846d8de59ef2
cianM167 <cianmcguire167@gmail.com>
2025-11-11 16:09:07 +0000

    Delete hello_world directory

--- hello_world/target/debug/deps/hello_world-d1577f69a8de56d2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cyan\projects\hello_world\target\debug\deps\hello_world-d1577f69a8de56d2.d: src\main.rs
-
-C:\Users\cyan\projects\hello_world\target\debug\deps\libhello_world-d1577f69a8de56d2.rmeta: src\main.rs
-
-src\main.rs:
--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cyan\projects\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-C:\Users\cyan\projects\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-src\main.rs:
